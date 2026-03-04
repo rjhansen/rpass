c4c8b3ab2ecb15b41bcf7e1223d619ad8f9fd10d
Robert J. Hansen <rob@hansen.engineering>
2026-03-04 12:19:26 -0500

    Now matches `pwgen` functionality.

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -18,36 +18,67 @@ use std::process::exit;
 "
 )]
 pub struct Args {
-    #[arg(
-        short,
-        long,
-        help = "bits of entropy per password",
-        default_value_t = 128
-    )]
-    pub(crate) bits: u16,
+    #[arg(short = 'c', long = "capitalize", help = "ensure one or more capital letters", default_value_t = false)]
+    pub(crate) ensure_capitals: bool,
 
-    #[arg(
-        short,
-        long,
-        help = "number of passwords to generate",
-        default_value_t = 1
-    )]
-    pub(crate) count: u32,
+    #[arg(short = 'A', long = "no-capitalize", help = "ensure no capital letters", default_value_t = false)]
+    pub(crate) no_capitals: bool,
+
+    #[arg(short = 'n', long = "numerals", help = "ensure one or more numbers", default_value_t = false)]
+    pub(crate) ensure_numbers: bool,
+
+    #[arg(short = '0', long = "no-numerals", help = "ensure no numbers", default_value_t = false)]
+    pub(crate) no_numbers: bool,
+
+    #[arg(short = 'y', long = "symbols", help = "ensure at least one special character", default_value_t = false)]
+    pub(crate) ensure_symbols: bool,
+
+    #[arg(short = 'r', long = "remove-chars", help = "omit these characters", default_value_t = String::new())]
+    pub(crate) remove_set: String,
+
+    #[arg(short = 's', long = "secure", help = "use a cryptographic-strength RNG", default_value_t = true)]
+    pub(crate) secure: bool,
+
+    #[arg(short = 'B', long = "ambiguous", help = "don't include ambiguous characters", default_value_t = false)]
+    pub(crate) no_ambiguous: bool,
+
+    #[arg(short = 'v', long = "no-vowels", help = "don't use any vowels", default_value_t = false)]
+    pub(crate) no_vowels: bool,
 
     #[arg(long = "copyright", help = "show copyright notice")]
-    copyright: bool,
+    pub(crate) copyright: bool,
 
-    #[arg(long = "bugs", help = "show contact information for bug reports")]
-    bugs: bool,
+    #[arg(short = '1', long = "one-column", help = "display results in one column", default_value_t = false)]
+    pub(crate) one_column: bool,
+
+    #[arg(short = 'C', long = "multicolumn", help = "display results in multiple columns", default_value_t = true)]
+    pub(crate) multi_column: bool,
+
+    #[arg(short = 'b', long = "bugs", help = "show contact information for bug reports")]
+    pub(crate) bugs: bool,
+
+    pub(crate) length: Option<u16>,
+    pub(crate) count: Option<u16>,
 }
 
 pub fn parse_command_line() -> Args {
-    let rv = Args::parse();
-    sanity_checks(&rv);
+    let mut rv = Args::parse();
+    sanity_checks(&mut rv);
     rv
 }
 
-fn sanity_checks(args: &Args) {
+fn sanity_checks(args: &mut Args) {
+    args.multi_column = true;
+
+    if args.one_column {
+        args.multi_column = false;
+    }
+
+    if (args.ensure_capitals && args.no_capitals) || (args.ensure_numbers && args.no_numbers) {
+        eprintln!("error: conflicting flags");
+        exit(1);
+    }
+
     if args.bugs {
         println!("Submit bugs on GitHub: https://github.com/rjhansen/rpass");
         exit(0);
@@ -62,12 +93,14 @@ fn sanity_checks(args: &Args) {
         println!("Released under the Apache 2.0 license. Share and enjoy!");
         exit(0);
     }
-    if (0 != (args.bits % 8)) || (args.bits < 64) || (args.bits > 256) {
-        eprintln!("error: bits must be a multiple of eight in the range [64, 256].");
+    let length = args.length.unwrap_or_else(|| 8);
+    let count = args.count.unwrap_or_else(|| 1);
+    if length < 6 || length > 43 {
+        eprintln!("error: length must be in the range [6, 43].");
         exit(1);
     }
-    if args.count > 20 || args.count < 1 {
-        eprintln!("error: count must be in range [1, 20].");
+    if count < 1 || count > 1000 {
+        eprintln!("error: count must be in range [1, 1000].");
         exit(1);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,132 @@
 pub mod cmdline;
 
+use terminal_size::{terminal_size, Height, Width};
 use base64::engine::general_purpose;
-use base64::{alphabet, engine::GeneralPurpose, Engine as _};
+use base64::{Engine as _, alphabet, engine::GeneralPurpose};
 use cmdline::parse_command_line;
-use rand::{Rng, SeedableRng};
+use rand::{Rng, RngExt, SeedableRng};
 use rand_hc::Hc128Rng;
+use std::collections::HashSet;
 use zeroize::Zeroize;
+
+fn make_filter(args: &cmdline::Args) -> impl Fn(&char) -> bool {
+    let mut set = HashSet::<char>::new();
+    let mut vowels = HashSet::<char>::new();
+    let mut ambiguous = HashSet::<char>::new();
+    args.remove_set.chars().for_each(|x| {
+        _ = &set.insert(x);
+    });
+    "01aeiouyAEIOUY"
+        .to_string()
+        .chars()
+        .into_iter()
+        .for_each(|x| {
+            _ = vowels.insert(x);
+        });
+    "B8G6I1l0OQDS5Z2"
+        .to_string()
+        .chars()
+        .into_iter()
+        .for_each(|x| {
+            _ = ambiguous.insert(x);
+        });
+    move |ch: &char| -> bool {
+        !(args.no_capitals && ch.is_ascii_uppercase())
+            || (args.no_numbers && ch.is_ascii_digit())
+            || (args.no_vowels && vowels.contains(ch))
+            || (args.no_ambiguous && ambiguous.contains(ch))
+            || set.contains(ch)
+    }
+}
 
 fn main() {
     let args = parse_command_line();
+
+    const SYMBOLS: &str = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    const CAPITALS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const BUFFER_SIZE: usize = 128;
     const ENGINE: GeneralPurpose =
         GeneralPurpose::new(&alphabet::STANDARD, general_purpose::NO_PAD);
-    let mut buf = vec![0u8; (args.bits as usize) / 8];
+
+    let mut buf = [0u8; BUFFER_SIZE];
     let mut csprng = Hc128Rng::from_rng(&mut rand::rng());
+    csprng.fill_bytes(&mut buf);
+    let mut pool = ENGINE.encode(&buf);
+    buf.zeroize();
+    let mut iterator = pool.chars();
+    let filter = make_filter(&args);
+    let mut count: u16 = args.count.unwrap_or_else(|| 1);
+    let length: u16 = args.length.unwrap_or_else(|| 8);
+    let line_width = match terminal_size() {
+        Some((Width(w), Height(_))) => if w < 160 { w } else { 160 },
+        None => 80
+    };
+    let words_per_line = ((line_width as f32 / (length + 1) as f32).ceil() as u16) - 2;
+    let mut remaining_in_this_line = words_per_line;
 
-    for _ in 0..args.count {
-        csprng.fill_bytes(&mut buf);
-        println!("{}", ENGINE.encode(&buf));
-
-        // The password needs to be securely scrubbed in a way the
-        // code optimizer won't optimize away. The zeroize crate
-        // comes to our rescue.
-        buf.zeroize();
+    if args.count.is_none() && args.multi_column {
+        count = (words_per_line + 1) * 20;
     }
+
+    for index in 0..count {
+        let mut remaining = length;
+        let mut password = "".to_string();
+
+        if args.ensure_symbols {
+            password.push(SYMBOLS
+                .chars()
+                .nth(csprng
+                    .random_range(0..SYMBOLS.chars().count()))
+                .unwrap_or_else(|| '+'));
+            remaining -= 1;
+        }
+        if args.ensure_capitals {
+            password.push(CAPITALS
+                .chars()
+                .nth(csprng
+                    .random_range(0..CAPITALS.chars().count()))
+                .unwrap_or_else(|| 'A'));
+            remaining -= 1;
+        }
+        while remaining > 0 {
+            let mut ch = match iterator.next() {
+                Some(x) => {
+                    match filter(&x) {
+                        true => x,
+                        false => continue,
+                    }
+                }
+                None => {
+                    pool.zeroize();
+                    csprng.fill_bytes(&mut buf);
+                    pool = ENGINE.encode(&buf);
+                    buf.zeroize();
+                    iterator = pool.chars();
+                    continue;
+                }
+            };
+            password.push(ch);
+            ch.zeroize();
+            remaining -= 1;
+        }
+
+        if args.multi_column && !args.one_column {
+            if remaining_in_this_line > 0 {
+                if index < (count - 1) {
+                    print!("{} ", password);
+                } else {
+                    println!("{}", password);
+                }
+                remaining_in_this_line -= 1;
+            } else {
+                println!("{}", password);
+                remaining_in_this_line = words_per_line;
+            }
+        } else {
+            println!("{}", password);
+        }
+        password.zeroize();
+    }
+    pool.zeroize();
+    buf.zeroize();
 }
