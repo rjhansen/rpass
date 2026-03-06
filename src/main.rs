45805ca30c8de6a184757d363b97c67eda2881be
Robert J. Hansen <rob@hansen.engineering>
2026-03-06 08:52:55 -0500

    Now correctly reports a warning message to stderr if the -s flag is specified.

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,12 @@ use zeroize::Zeroize;
 
 fn main() {
     let args = parse_command_line();
-    match args.secure {
-        true => eprintln!(
+    if args.secure {
+        eprintln!(
 r#"info: the -s flag is unnecessary. pwgen would by default create passwords
       from phonemes, but by passing -s it would abandon phonemes in favor
       of high-entropy random glyphs. rpass only generates high-entropy
-      random glyphs. You may safely drop this flag from your pipeline."#),
-        false => {}
+      random glyphs. You may safely drop this flag from your pipeline."#);
     }
 
     // The password generator closure uses some sensitive memory which
